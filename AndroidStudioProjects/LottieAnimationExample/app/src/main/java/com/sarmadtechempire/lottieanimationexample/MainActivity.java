9d315ea04e2fb0f9080f8da8370ea4bf0bfe7efd
isarmadfarooq <i.sarmadfarooq@gmail.com>
2024-06-01 21:25:54 +0500

    In that projaect learn how to impement custom animation using Lottie library and do implementat by creating app

---
+++ AndroidStudioProjects/LottieAnimationExample/app/src/main/java/com/sarmadtechempire/lottieanimationexample/MainActivity.java
@@ -0,0 +1,33 @@
+package com.sarmadtechempire.lottieanimationexample;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class MainActivity extends AppCompatActivity {
+
+//    LottieAnimationView ltView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+//        ltView = findViewById(R.id.ltView);
+//
+//        ltView.setAnimation(R.raw.lottie);
+//        ltView.playAnimation();
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
