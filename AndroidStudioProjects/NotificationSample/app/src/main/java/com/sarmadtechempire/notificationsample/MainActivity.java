98cd78b11a2049c3ee66426d0ff448833289919f
isarmadfarooq <i.sarmadfarooq@gmail.com>
2024-06-21 10:30:58 +0500

    Apply different styles on Notifications

--- AndroidStudioProjects/NotificationSample/app/src/main/java/com/sarmadtechempire/notificationsample/MainActivity.java
+++ AndroidStudioProjects/NotificationSample/app/src/main/java/com/sarmadtechempire/notificationsample/MainActivity.java
@@ -3,6 +3,9 @@ package com.sarmadtechempire.notificationsample;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.om.FabricatedOverlay;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -18,7 +21,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     private static final String CHANNEL_ID = "channel_id";
-    private static final Integer NOTIFICATION_ID = 100;
+    private static final int NOTIFICATION_ID = 100;
+    private static final int REQUEST_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +39,49 @@ public class MainActivity extends AppCompatActivity {
 
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         Notification notification;
+
+        Intent iNotify = new Intent(MainActivity.this, MainActivity.class);
+        iNotify.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pi = PendingIntent.getActivity(MainActivity.this, REQUEST_CODE, iNotify, PendingIntent.FLAG_UPDATE_CURRENT );
+
+        //BigPicture Style
+        Notification.BigPictureStyle bigPictureStyle = new Notification.BigPictureStyle()
+                .bigPicture(((BitmapDrawable) (ResourcesCompat.getDrawable(getResources(), R.drawable.new_contact_add_icon, null))).getBitmap())
+                .bigLargeIcon(largeIcon)
+                .setBigContentTitle("Image sent by Ali")
+                .setSummaryText("Image Message");
+
+        //InBox Style
+        Notification.InboxStyle inboxStyle = new Notification.InboxStyle()
+                .addLine("A")
+                .addLine("B")
+                .addLine("C")
+                .addLine("D")
+                .addLine("E")
+                .addLine("F")
+                .addLine("G")
+                .addLine("H")
+                .addLine("I")
+                .addLine("J")
+                .addLine("K")
+                .addLine("L")
+                .addLine("M")
+                .setBigContentTitle("Full Message")
+                .setSummaryText("Message from Sarmad");
+
+
+
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             notification = new Notification.Builder(MainActivity.this)
                     .setLargeIcon(largeIcon)
                     .setSmallIcon(R.drawable.app_icon)
                     .setContentText("New Contact")
+                    .setAutoCancel(false)
                     .setSubText("You have a new contact")
+                    .setContentIntent(pi)
+                    .setStyle(inboxStyle)
+                    .setOngoing(true)
                     .setChannelId(CHANNEL_ID)
                     .build();
             notificationManager.createNotificationChannel(new NotificationChannel(CHANNEL_ID,"New Channel",NotificationManager.IMPORTANCE_HIGH));
@@ -50,7 +91,11 @@ public class MainActivity extends AppCompatActivity {
                     .setLargeIcon(largeIcon)
                     .setSmallIcon(R.drawable.app_icon)
                     .setContentText("New Contact")
+                    .setAutoCancel(false)
                     .setSubText("You have a new contact")
+                    .setContentIntent(pi)
+                    .setStyle(inboxStyle)
+                    .setOngoing(true)
                     .build();
         }
 
